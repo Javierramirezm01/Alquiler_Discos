0507476344fb4342050f47b151bad5d79c5b63d5
anyi7777 <zapatarealpeanyi@gmail.com>
2021-03-06 21:31:09 -0500

    fix commit

--- Alquiler_Discos/Models/Alquiler.cs
+++ Alquiler_Discos/Models/Alquiler.cs
@@ -7,5 +7,15 @@ namespace Alquiler_Discos.Models
 {
     public class Alquiler
     {
+        public int Id { get; set; }
+
+        public string nroAlquiler { get; set; }
+        public string fechaAlquiler { get; set; }
+        public string valorAlquiler { get; set; }
+
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        
     }
 }
--- Alquiler_Discos/Models/DetalleAlquiler.cs
+++ Alquiler_Discos/Models/DetalleAlquiler.cs
@@ -7,5 +7,16 @@ namespace Alquiler_Discos.Models
 {
     public class DetalleAlquiler
     {
+        public int Id { get; set; }
+
+        public string item { get; set; }
+        public string diasPrestamo { get; set; }
+        public string fechaDevolucion { get; set; }
+
+        public int CdId { get; set; }
+        public Cd Cd { get; set; }
+
+        public int AlquilerId { get; set; }
+        public Alquiler Alquiler { get; set; }
     }
 }
