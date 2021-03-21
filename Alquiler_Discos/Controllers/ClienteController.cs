23b6d4b24830ecd9e8d7d5f8f36ae0f933c482c6
zrealpe <andres.zapata676@pascualbravo.edu.co>
2021-03-21 12:40:37 -0500

    Act Controladores Sancion || Cliente

---
+++ Alquiler_Discos/Controllers/ClienteController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alquiler_Discos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly Context _miBd;
+
+        public ClienteController(Context miBd)
+        {
+            _miBd = miBd;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                var Lista = _miBd.clientes.ToList();
+                oRespuesta.Exito = 1;
+                oRespuesta.Datos = Lista;
+
+            }
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteViewModel oCliente)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                Cliente cliente = new Cliente();
+                cliente.Nombre = oCliente.Nombre;
+                cliente.Direccion = oCliente.Direccion;
+                cliente.Telefono = oCliente.Telefono;
+                cliente.Email = oCliente.Email;
+                cliente.FechaNacimineto = oCliente.FechaNacimineto;
+                cliente.FechaInscripcion = oCliente.FechaInscripcion;
+                cliente.TemaInteres = oCliente.TemaInteres;
+                cliente.Estado = oCliente.Estado;
+                cliente.NroDNI = oCliente.NroDNI;
+                _miBd.clientes.Add(cliente);
+                _miBd.SaveChanges();
+                oRespuesta.Exito = 1;
+            }
+
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Update(ClienteViewModel oCliente)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                Cliente cliente = new Cliente();
+                cliente.Nombre = oCliente.Nombre;
+                cliente.Direccion = oCliente.Direccion;
+                cliente.Telefono = oCliente.Telefono;
+                cliente.Email = oCliente.Email;
+                cliente.FechaNacimineto = oCliente.FechaNacimineto;
+                cliente.FechaInscripcion = oCliente.FechaInscripcion;
+                cliente.TemaInteres = oCliente.TemaInteres;
+                cliente.Estado = oCliente.Estado;
+                cliente.NroDNI = oCliente.NroDNI;
+                _miBd.clientes.Add(cliente);
+                _miBd.SaveChanges();
+                oRespuesta.Exito = 1;
+
+
+            }
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                var cliente = _miBd..Find(Id);
+                _miBd.clientes.Remove(cliente);
+                _miBd.SaveChanges();
+                oRespuesta.Exito = 1;
+
+
+            }
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+    }
+}
---
+++ Alquiler_Discos/Controllers/SancionController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alquiler_Discos.Models;
+using Alquiler_Discos.Response;
+using Alquiler_Discos.ViewModels;
+
+namespace Alquiler_Discos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SancionController : ControllerBase
+    {
+        private readonly Context _miBd;
+
+        public SancionController(Context miBd)
+        {
+            _miBd = miBd;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                var Lista = _miBd.sancions.ToList();
+                oRespuesta.Exito = 1;
+                oRespuesta.Datos = Lista;
+
+            }
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(SancionViewModel oCliente)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                Sancion sancion = new Sancion();
+                sancion.TipoSancion = oSancion.TipoSancion;
+                sancion.NroDiasSancion = oSancion.NroDiasSancion;
+                _miBd.sancions.Add(sancion);
+                _miBd.SaveChanges();
+                oRespuesta.Exito = 1;
+            }
+
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Update(SancionViewModel oCliente)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                Sancion sancion = new Sancion();
+                sancion.TipoSancion = oSancion.TipoSancion;
+                sancion.NroDiasSancion = oSancion.NroDiasSancion;
+                _miBd.sancions.Add(sancion);
+                _miBd.SaveChanges();
+                oRespuesta.Exito = 1;
+
+
+            }
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                var sancion = _miBd.sancions.Find(Id);
+                _miBd.clientes.Remove(sancion);
+                _miBd.SaveChanges();
+                oRespuesta.Exito = 1;
+
+
+            }
+            catch (Exception e)
+            {
+                oRespuesta.Mensaje = e.Message;
+
+            }
+
+            return Ok(oRespuesta);
+        }
+
+    }
+}
